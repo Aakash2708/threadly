cd36cdfef64e679af4a0d25c24332fb37c76a503
Aakash847 <tyagiaakash.mypc345@gmail.com>
2025-04-21 15:39:29 +0530

    Deleted

--- Backend/x/src/main/java/com/x/Config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.x.Config;
-
-public class JwtConstant {
-	
-	public static final String SECRET_KEY="dshgydvhvhvhvdhvhgvhdvcdgdcvgcvdshvcgdvgdhbfdhcbdhcvdhcbdhcvdhcvdhvchdvcdbcjd";
-	public static final String JWT_HEADER ="Authorization";
-	
-
-}
